dced279ded108b553fbd921daf4098bccf56215d
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-05-11 16:54:45 +0200

    The compiler does not see the UI testing hooks category during release builds.

--- Classes/L0MoverAppDelegate+L0UITestingHooks.h
+++ Classes/L0MoverAppDelegate+L0UITestingHooks.h
@@ -12,9 +12,10 @@
 #import "L0MoverAppDelegate.h"
 
 
+#if DEBUG
 @interface L0MoverAppDelegate (L0UITestingHooks)
 
-#if DEBUG
+
 - (void) testWelcomeAlert;
 - (void) testContactTutorialAlert;
 - (void) testImageTutorialAlert;
@@ -22,6 +23,6 @@
 - (void) testNewVersionAlert;
 - (void) testNetworkBecomingUnavailable; // WARNING: Disables network watching, use with care.
 - (void) testNetworkBecomingAvailable; // WARNING: Disables network watching, use with care.
-#endif
 
 @end
+#endif
